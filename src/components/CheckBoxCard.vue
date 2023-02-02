9021ae7ee218f6e2906c234811bdb4a02841f7c2
sooryasrajan <soorya.s27@gmail.com>
2023-02-02 21:21:22 +0530

    Added check box card Added AddEditMemberModal.vue to manage adding and editing users Added a white elevated button

---
+++ src/components/CheckBoxCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="check-box-card">
+    <div class="row-el">
+      <input type="checkbox" v-model="checked">
+    </div>
+    <div class="row-el">
+      <div class="title">
+        {{title}}
+      </div>
+      <div class="sub-title">
+        {{subtitle}}
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue} from 'vue-class-component';
+import {Prop} from 'vue-property-decorator/lib/decorators/Prop';
+
+export default class HelloWorld extends Vue {
+
+  @Prop() private title!: string;
+  @Prop() private subtitle!: string;
+  @Prop() private checked!: boolean;
+
+}
+</script>
+
+<style scoped lang="scss">
+
+.check-box-card{
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border-bottom: 1px solid #f0ecf4;
+  padding-top: 10px;
+  padding-bottom: 20px;
+
+  input{
+    width: 17px;
+    height: 17px;
+  }
+
+  .row-el:nth-child(2){
+    margin-left: 10px;
+
+    .title{
+      font-size: 18px;
+      font-weight: 500;
+      color: #1c1c1c;
+    }
+
+    .sub-title{
+      margin-top: 6px;
+      font-size: 15px;
+      font-weight: 500;
+      color: #5e5e5e;
+    }
+
+  }
+
+}
+
+</style>
---
+++ src/components/ElevatedButtonWhite.vue
@@ -0,0 +1,52 @@
+<template>
+  <button @click="onClick">
+    <div class="button-flex">
+      <div>
+        <slot></slot>
+      </div>
+    </div>
+  </button>
+</template>
+
+
+<script lang="ts">
+import {Vue} from "vue-class-component";
+import {Prop} from 'vue-property-decorator/lib/decorators/Prop';
+
+export default class ElevatedButtonWhite extends Vue {
+
+  @Prop() private onClick!: () => void;
+
+}
+
+</script>
+
+<style scoped>
+button {
+  min-width: 130px;
+  background-color: white;
+  color: #5a1fcb;
+  padding: 8px 35px 8px 35px;
+  border-radius: 5px;
+  border: none;
+  font-size: 15px;
+  font-weight: 400;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.2s ease-in-out;
+}
+
+.button-flex{
+  display: flex;
+  width: max-content;
+  align-items: center;
+  justify-content: center;
+}
+
+button:hover {
+  background-color: rgba(90, 31, 203, 0.38);
+}
+
+</style>
